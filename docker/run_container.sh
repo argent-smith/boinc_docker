21cb82b168a3273462f59a0cc1b16499c6df1cd4
Pavel Argentov <argentoff@gmail.com>
2016-04-13 00:19:26 +0300

    Configuration variables; container launch pipeline improvements

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -6,12 +6,19 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 if [ "$1" = 'boinc' ]; then
+    if [ ! "$(cat /etc/timezone)" = "${TIMEZONE}" ]; then
+        echo "${TIMEZONE}" > /etc/timezone \
+        && dpkg-reconfigure -f noninteractive tzdata
+    fi
+
+    echo "${BOINC_GUI_PASSWORD}" > /etc/boinc-client/gui_rpc_auth.cfg
+
     mkdir -p ${BOINC_HOME}
-    chmod 700 ${BOINC_HOME}
+    chmod 755 ${BOINC_HOME}
     chown -R ${BOINC_USER}:${BOINC_GROUP} ${BOINC_HOME}
 
     cd ${BOINC_HOME}
-    exec gosu ${BOINC_USER} "$@"
+    exec gosu ${BOINC_USER} /bin/sh -c "$*"
 fi
 
 exec "$@"
